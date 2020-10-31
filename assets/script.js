c2be089912fd7e1b4dade9f7d0d4cf4e3463fd77
Waterbug <amoseman18@gmail.com>
2020-10-31 11:38:58 -0600

    fixed first api call

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
- var randomREl = document.getElementById("#randomR");
- var recipeEl = document.getElementById("#recipeCard")   
+  var randomREl = document.getElementById("#randomR");
+  var recipeEl = document.getElementById("#recipeCard")   
+  $("#randomR").on("click", getRecipeRepos);
 function getRecipeRepos() {
+  console.log('inside getRecipeRepos')
     var requestUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
     fetch(requestUrl)
     .then(function (response) {
@@ -9,26 +11,14 @@ function getRecipeRepos() {
     })
     .then(function (data) {
       console.log(data);
-     var randomBtn = $("<button>").text(data.strMeal + data.strInstructions)
-       $("#randomR").append(randomBtn);
+     var randomEl = $("<div>").text(data.meals[0].strMeal + data.meals[0].strInstructions)
+        $("#recipeCard").append(randomEl);
       
    })
 
 
 }
-  
+   
   });
-  $("#randomR").on("click", getRecipeRepos);
+  
 
-$(document).ready(function () {
-
-
-
-
-
-
-
-
-
-
-})
