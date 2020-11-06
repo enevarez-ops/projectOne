d882c2ac493451a57f921d2d421862103077c933
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2020-11-05 23:19:48 -0600

    finally

--- assets/recipes.js
+++
@@ -1,3 +0,0 @@
-//document to load recipes
-// function that produces 10 recipes from random recipe list. Must choose recipes first
-//for loop through the array of picked recipes to display in list 
--- assets/script.js
+++ assets/script.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
   $("#randomR").on("click", callFunction);
   
   function callFunction() {
-    //console.log("inside callFunction");
+    
     getRandomMusic();
     getRecipeRepos();
    
-  }
+  };
 
   function getRecipeRepos() {
-  //console.log('inside getRecipeRepos')
+  
     var requestUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
     fetch(requestUrl)
     .then(function (response) {
@@ -21,82 +21,74 @@ $(document).ready(function(){
     })
     .then(function (data) {
       console.log(data);
-     var randomEl = $("<a>").text("YouTube: " + data.meals[0].strYoutube);
-        $("#recipeChoice").append(randomEl);
+        var randomEl = $("<a>").text("YouTube: " + data.meals[0].strYoutube);
         var recipeImg = $("<img>").attr("src", data.meals[0].strMealThumb);
         var recipeSrc = $("<a>").text("" + data.meals[0].strMeal).attr("href", data.meals[0].strSource).attr("target", "_blank");
         var recipeCat = $("<h3>").text("Category: " + data.meals[0].strCategory).attr("header", data.meals[0].strCategory);
-        //targeting a new page with link
-         recipeSrc.attr("target", "_blank");
-        $("#recipePic").append(recipeImg);
-        $("#recipeLink").append(recipeSrc);
+        localStorage.setItem("recipe", data.meals[0].strMeal);
+        recipeSrc.attr("target", "_blank");
+        $("#recipeVid").empty();
+        $("#recipeVid").append(randomEl);
+        $("#recipeCat").empty();
         $("#recipeCat").append(recipeCat);
-        // $("#recipePic").empty();
-        // $("#recipeLink").empty();
-        // $("#recipeChoice").empty();
+        $("#recipeLink").empty();
+        $("#recipeLink").append(recipeSrc);
+        $("#recipePic").append(recipeImg);
       
-   }) //add category and ingredients to .then 
+      
+   }) 
 
-  }
-
-//var songArry = ["lose yourself", "other song"]
-//function getsRandomSong() {s
-  // get a random song title from Song array
-
-  //then call call getRandomMusic("Lose Yourself")
-
-
-
+  };
 
   function getRandomMusic() {
-fetch("https://deezerdevs-deezer.p.rapidapi.com/playlist//us/playlist/1363560485", {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-    "x-rapidapi-key": "748a15681fmshbba4031511d91cep19839fjsnbcb11ed54479"
-}})
-.then(response => {
-  console.log(response);
+    const settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://deezerdevs-deezer.p.rapidapi.com/playlist/908622995",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "2d25421a49msh9521948ccae11a9p147deejsnab4c9aa7d4f2",
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+      }
+    };
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+    $.ajax(settings).done(function (response) {
+      console.log(response.tracks.data[Math.floor(Math.random(40))]);
+      var newObj = response.tracks.data[Math.floor(Math.random() * 10)];
+      console.log('album', newObj)
+      // var title = newObj.title;
+      var albumtitle = newObj.album.title
+      var artistName = newObj.artist.name
+      var link = $('<a>').attr('href', newObj.link).attr('title', 'album link').text(newObj.album.title);
+      // var button = $('<div>').val(newObj.album.title)
+      var newDiv = $("<div>").addClass("musicDisplay");
+      newDiv.append("Album: " + albumtitle);
+      $("#musicChoice").empty();
+      $("#musicChoice").append(newDiv);
+      $("#musicTitle").empty();
+      $("#musicTitle").append(link);
+      $("#musicArtist").empty();
+      $("#musicArtist").append(artistName);
 
-  return response.json();
-  
-})
-.then(function(data) {
-  // console.log(data.data[Math.floor(Math.random() * 10)]);
-  // var newObj = data.data[Math.floor(Math.random() * 10)];
-  // var title = newObj.title;
-  // var artist = newObj.artist.name;
-  // var img = $("<img>").attr("src", newObj.album.cover);
-  // var newDiv = $("<div>").addClass("musicDisplay");
-  // newDiv.append(title, artist, img);
-  // $("#musicChoice").append(newDiv);
-})
-}
+    });
+  }
+
+
+  $('#musicChoice').on('click', 'button', function (e) {
+    e.preventDefault()
+
+    var songs = JSON.parse(window.localStorage.getItem("songs")) || [];
+
+    var value = $('#saveBtn').val()
+    console.log(value)
+    var newSong = {
+      value
+    }
+    songs.push(newSong)
+    localStorage.setItem('songs', JSON.stringify(songs))
+  })
 
 });
-  
-  
-  
-  
-  // for (i = 0; i <  data.length; i++){
-  // var randomMusicEl = $("<div>").text("Your song: " + data[i].artist.name + " " + data[i].link);
-  // $("#musicChoice").append(randomMusicEl);
-//}
 
-
-//music button appending
-// var musicList = ["https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", "https://deezerdevs-deezer.p.rapidapi.com/search?q=shakira", "https://deezerdevs-deezer.p.rapidapi.com/search?q=jurrasicfive"];
-// for (var i = 0; i< musicList.length; i++) {
-//   var artistList = musicList[i];
-//   console.log(musicList[i]);
-//   artistList.text(musicList[i]);
-//   $("#musicButton").append(artistList);
-// }
-
-//try catch then error if it does not hit the api
-// .catch(err => {
-// 	console.log(err);
-
-      // var randomMusicEl = $("<div>").text("Your song: " + data[0].artist.name + " " + data[0].link);
-      // $("musicChoice").append(randomMusicEl);
-    // });
